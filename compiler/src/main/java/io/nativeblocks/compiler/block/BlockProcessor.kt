cc37226ea35392332ad4e202df5bef85abe99e55
alireza <alireza@nativeblocks.io>
2024-07-24 16:35:08 +0400

    find all functions that annotated with block symbol

--- compiler/src/main/java/io/nativeblocks/compiler/block/BlockProcessor.kt
+++ compiler/src/main/java/io/nativeblocks/compiler/block/BlockProcessor.kt
@@ -4,11 +4,21 @@ import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
+
+private const val BLOCK_ANNOTATION_SYMBOL = "io.nativeblocks.core.type.NativeBlock"
 
 class BlockProcessor(private val environment: SymbolProcessorEnvironment) : SymbolProcessor {
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        TODO("Not yet implemented")
+        val symbols = resolver
+            .getSymbolsWithAnnotation(BLOCK_ANNOTATION_SYMBOL)
+            .filterIsInstance<KSFunctionDeclaration>()
+
+        if (!symbols.iterator().hasNext()) return emptyList()
+        val sources = resolver.getAllFiles().toList().toTypedArray()
+
+        return listOf()
     }
 
 }
