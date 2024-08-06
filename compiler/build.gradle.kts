4f7fb57b1837c36a127ce6b1c4273c8d0dc1aeba
alireza <alireza@nativeblocks.io>
2024-08-06 14:43:12 +0300

    add publishing script for compiler library

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.vanniktech.maven.publish")
 }
 
 java {
@@ -9,8 +14,51 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    coordinates(ModuleInfo.GROUP_ID, ModuleInfo.ARTIFACT_ID, ModuleInfo.VERSION)
+    signAllPublications()
+    configure(
+        JavaLibrary(
+            javadocJar = JavadocJar.Javadoc(),
+            sourcesJar = true,
+        )
+    )
+
+    pom {
+        name.set(ModuleInfo.ARTIFACT_ID)
+        description.set(ModuleInfo.DESCRIPTION)
+        url.set(ModuleInfo.URL)
+        licenses {
+            license {
+                name = "NATIVEBLOCKS TERMS OF SERVICE"
+                url = "https://nativeblocks.io/terms-of-service"
+            }
+        }
+        developers {
+            developer {
+                name = "Nativeblocks"
+                email = "dev@nativeblocks.io"
+            }
+        }
+        scm {
+            connection = "scm:git:github.com/nativeblocks/nativeblocks-android.git"
+            developerConnection =
+                "scm:git:ssh://github.com/nativeblocks/nativeblocks-android.git"
+            url = "https://github.com/nativeblocks/nativeblocks-android"
+        }
+    }
+}
+
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.22-1.0.17")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("com.squareup:kotlinpoet:1.18.1")
+}
+
+object ModuleInfo {
+    const val GROUP_ID = "io.nativeblocks"
+    const val ARTIFACT_ID = "nativeblocks-compiler-android"
+    const val VERSION = "1.0.1"
+    const val DESCRIPTION = "Nativeblocks compiler SDK for Android"
+    const val URL = "https://nativeblocks.io"
 }
