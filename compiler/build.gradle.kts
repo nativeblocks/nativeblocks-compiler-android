7bca316fc0dc16bd3e4da9ff51504d1735bb9a04
alireza <alireza@nativeblocks.io>
2024-08-08 18:04:39 +0300

    update version to 1.0.2

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 object ModuleInfo {
     const val GROUP_ID = "io.nativeblocks"
     const val ARTIFACT_ID = "nativeblocks-compiler-android"
-    const val VERSION = "1.0.1"
+    const val VERSION = "1.0.2"
     const val DESCRIPTION = "Nativeblocks compiler SDK for Android"
     const val URL = "https://nativeblocks.io"
 }
