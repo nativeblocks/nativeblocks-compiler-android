e1c33eafd02942cb47edb01c8f447cd7e47522eb
alireza <alireza@nativeblocks.io>
2024-10-20 15:54:45 +0300

    update kotlin to 1.9.24 and compose to 1.5.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application").version("8.4.1").apply(false)
     id("com.android.library").version("8.4.1").apply(false)
-    id("org.jetbrains.kotlin.android").version("1.9.22").apply(false)
-    id("org.jetbrains.kotlin.plugin.serialization").version("1.9.22").apply(false)
-    id("com.google.devtools.ksp").version("1.9.22-1.0.17").apply(false)
-    id("org.jetbrains.kotlin.jvm").version("1.9.22").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.24").apply(false)
+    id("org.jetbrains.kotlin.plugin.serialization").version("1.9.24").apply(false)
+    id("com.google.devtools.ksp").version("1.9.24-1.0.20").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("1.9.24").apply(false)
     id("com.vanniktech.maven.publish").version("0.29.0").apply(false)
 }
